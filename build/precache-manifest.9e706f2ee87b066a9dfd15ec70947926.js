af643f102e2a9268212fff9c0519879fb52066ae
Adif Dwi Maulana <adifdwimaulana@gmail.com>
2019-06-08 08:16:56 +0700

    Initial commit to Github Page

---
+++ build/precache-manifest.9e706f2ee87b066a9dfd15ec70947926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0ab04fc30c59d9de1dec2c41e0ec11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "518ff7facf2de6f1e7d3",
+    "url": "/static/css/main.f53eafac.chunk.css"
+  },
+  {
+    "revision": "8650746c3ddff8b26e02",
+    "url": "/static/js/2.2953743d.chunk.js"
+  },
+  {
+    "revision": "518ff7facf2de6f1e7d3",
+    "url": "/static/js/main.4ab2ab6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
